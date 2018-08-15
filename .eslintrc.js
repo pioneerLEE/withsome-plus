a72b486feaa2965122ada7f0d5ed036a3ed30321
ParkSB <parkgds@gmail.com>
2018-08-15 15:50:18 +0900

    chore: eslint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "linebreak-style": 0,
+        "comma-dangle": 0,
+        "class-methods-use-this": 0,
+        "import/no-cycle": 0,
+        "operator-linebreak": 0,
+        "react/jsx-filename-extension": 0,
+        "react/prefer-stateless-function": 0,
+        "arrow-body-style": 0,
+    },
+};

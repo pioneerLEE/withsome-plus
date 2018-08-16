8e3b65966d58ccc48ae5a15a9792e418e4b91c06
ParkSB <parkgds@gmail.com>
2018-08-16 19:10:29 +0900

    refactor: TextStyle 추가 - FormStyle 제거.

---
+++ src/styles/TextStyle.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  h1Text: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  h2Text: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  h3Text: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+});
